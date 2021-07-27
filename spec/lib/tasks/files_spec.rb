335d04b627557f5e31ca451e3a301a99c5798dc5
Javi Martín <javim@elretirao.net>
2021-07-27 22:14:45 +0200

    Use Active Storage to handle cached attachments
    This fixes a few issues we've had for years.
    
    First, when attaching an image and then sending a form with validation
    errors, the image preview would not be rendered when the form was
    displayed once again. Now it's rendered as expected.
    
    Second, when attaching an image, removing it, and attaching a new
    one, browsers were displaying the image preview of the first one. That's
    because Paperclip generated the same URL from both files (as they both
    had the same hash data and prefix). Browsers usually cache images and
    render the cached image when getting the same URL.
    
    Since now we're storing each image in a different Blob, the images have
    different URLs and so the preview of the second one is correctly
    displayed.
    
    Finally, when users downloaded a document, they were getting files with
    a very long hexadecimal hash as filename. Now they get the original
    filename.

--- spec/lib/tasks/files_spec.rb
+++ spec/lib/tasks/files_spec.rb
@@ -11,36 +11,42 @@ describe "files tasks" do
       image = build(:image)
       document = build(:document)
 
-      image.attachment.save
-      document.attachment.save
+      image.storage_attachment.blob.save!
+      document.storage_attachment.blob.save!
+      image_file_path = image.file_path
+      document_file_path = document.file_path
 
       travel_to(2.days.from_now) { run_rake_task }
 
-      expect(File.exists?(image.attachment.path)).to be false
-      expect(File.exists?(document.attachment.path)).to be false
+      expect(File.exists?(image_file_path)).to be false
+      expect(File.exists?(document_file_path)).to be false
     end
 
     it "does not delete recent cached attachments" do
       image = build(:image)
       document = build(:document)
 
-      image.attachment.save
-      document.attachment.save
+      image.storage_attachment.blob.save!
+      document.storage_attachment.blob.save!
+      image_file_path = image.file_path
+      document_file_path = document.file_path
 
       travel_to(2.minutes.from_now) { run_rake_task }
 
-      expect(File.exists?(image.attachment.path)).to be true
-      expect(File.exists?(document.attachment.path)).to be true
+      expect(File.exists?(image_file_path)).to be true
+      expect(File.exists?(document_file_path)).to be true
     end
 
     it "does not delete old regular attachments" do
       image = create(:image)
       document = create(:document)
+      image_file_path = image.file_path
+      document_file_path = document.file_path
 
       travel_to(2.days.from_now) { run_rake_task }
 
-      expect(File.exists?(image.attachment.path)).to be true
-      expect(File.exists?(document.attachment.path)).to be true
+      expect(File.exists?(image_file_path)).to be true
+      expect(File.exists?(document_file_path)).to be true
     end
   end
 end
